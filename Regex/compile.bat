5dc90d6d7541c4c86675faba2a9cba176ffaf3f5
Kyle Hamilton <kyleih@optonline.net>
2013-12-07 16:44:31 -0500

    update scripts

--- Regex/compile.bat
+++ Regex/compile.bat
@@ -1,2 +1,2 @@
+set JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 javac -d bin src/edu/mccc/cos210/ds/*.java src/edu/mccc/cos210/regex/*.java src/edu/mccc/cos210/tests/*.java src/edu/mccc/cos210/ex/*.java
-#-Xlint:unchecked
--- Regex/run.bat
+++ Regex/run.bat
@@ -1,2 +1,1 @@
-export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
 java -cp bin; edu.mccc.cos210.regex.KHRETester
