442d1e0b80e46803a8ca7f155b89f57a7716df98
Kyle Hamilton <kyleih@optonline.net>
2013-12-08 14:33:35 -0500

    presentation

--- Regex/src/edu/mccc/cos210/regex/Regex.java
+++ Regex/src/edu/mccc/cos210/regex/Regex.java
@@ -42,11 +42,7 @@ public class Regex{
 
 		System.out.println("infix: "+regExpr);
 		System.out.println("posix: "+posix);
-		//System.out.println("language: "+itp.languageToString());
 		System.out.println("langStr: "+langStr);
-
-		
-
 
 		NFA nfa = new NFA(posix);
 		DFA dfa = new DFA(nfa, language);
@@ -62,11 +58,8 @@ public class Regex{
 	};
 	public boolean find(String regExpr, String target, int start) throws IOException, NullPointerException{
 
-		// JUST SOME STUBS:
-		ArrayListGraph dfa = parseRegex(regExpr);
 		//reset();
-		return eatString(dfa, target, start);
-		//return true;
+		return true;
 	};
 	public int getStart(){
 		return this.start;
@@ -90,6 +83,8 @@ public class Regex{
 		this.start = 0;
 	};
 	
+	// private methods:
+	// exact match:
 	private boolean eatDFA(int[][] table, String lang, ArrayList<Integer> fs, String target) throws IOException{
 
 		BufferedReader br = stringToBR(target);
@@ -97,12 +92,10 @@ public class Regex{
 		int state = 0;
 		while (((c = br.read()) != -1)) {
 			int input = lang.indexOf((char)c);
-			System.out.println("input: "+input); 
-			System.out.println("current state: "+state); 
-			if(input != -1){     
-				if (table[state][input] == -1){
-					return false; // Table indicates that this is not a valid input
-				}else if (fs.contains(table[state][input])){ // fs => final states in DFA
+			if(input != -1){ // if it's not in the language, then total fail
+				if (table[state][input] == -1){ // unreachable state
+					return false; 
+				}else if (fs.contains(table[state][input])  && !br.ready()){ // fs => final states in DFA
 					return true; // Table indicates that for this state, we accept the input given
 				}
 			// Advance to next state.
@@ -110,183 +103,16 @@ public class Regex{
 		    }else{
 		    	break;
 		    }  
-		    
 		}
 		return false;
 	}
-
-	// private methods:
-
-	// parse the regex string: make edges. edge has vertices and weight. list indexes are vertices. stack is weight.
-	private ArrayListGraph parseRegex(String regExpr) throws IOException {  
-
-		BufferedReader br = stringToBR(regExpr);
-		int c;
-
-		ArrayListGraph listGraph = new ArrayListGraph();
-
-		//ArrayList<Stack<String>> list = new ArrayList<Stack<String>>();
-
-		Stack<String> stack = new Stack<String>(); 
-
-		int counter = 0;
-		int startGroup = -1;
-		int endGroup = -1;
-
-		int state = 0;
-
-
-		while ((c = br.read()) != -1) {
-			// The special characters are: {}[]+*()|-
-			switch(c){
-				case '|' : //System.out.println("|");
-							state--;
-				break;
-				case '[' : System.out.println("["); // check what comes next: '-' or ':' or anything else, then process accorignly. (push chars onto same stack)
-				break;
-				case ']' : System.out.println("]");
-				break;
-				case '(' : //System.out.println("("); // start a new stack for each character that follows. keep track of position of '(' in case there is an '|'.
-							startGroup = counter;	
-				break;
-				case ')' : //System.out.println(")");
-							endGroup = counter;
-				break;
-				case '*' : System.out.println("*"); // always true. return to begining of group, and continue searching
-				break;
-				case '+' : System.out.println("+"); 
-				break;
-				case '-' : //System.out.println("-"); 
-				break;
-				case '{' : System.out.println("{"); 
-				break;
-				case '}' : System.out.println("}"); 
-				break;
-				case ':' : System.out.println("}"); 
-				break;
-				case '.' : System.out.println("."); 
-				break;
-				case '$' : System.out.println("$"); 
-				break;
-				case '^' : System.out.println("^"); 
-				break;
-				default : 
-
-					// make a new edge
-					//char[] ca = {(char)c};
-					listGraph.insert(new Edge(state, ++state, (char)c));
-			}
-			counter++;
-		}
-
-		br.close();
-		return listGraph;
-	}
-	private boolean matchString(ArrayListGraph dfa, String target) throws IOException{
-		BufferedReader br = stringToBR(target);
-		int c; 
-		int srcV = 0;
-		int destV;
-		while (((c = br.read()) != -1)) {
-			char nextChar = (char)c;
-			System.out.println("nextchar: "+nextChar);
-			Iterator<Edge> it = dfa.edgeIterator(srcV);
-
-			while(it.hasNext()){
-				Edge next = it.next();
-				System.out.println("next: "+next);
-				if(next.getWeight() == nextChar){
-					System.out.println("a match!: "+next);
-					srcV = next.getDest();
-				}else{
-					System.out.println("not a match!: "+next);
-					//continue;
-					//return false;
-				}
-			}
-		}
-
-		return true;
-	}
-
-
-	private boolean eatString(ArrayListGraph dfa, String target, int start) throws IOException{
-		System.out.println("eatme: "+start);
-		marker = start;
-		if(marker >= target.length()){
-			System.out.println("it's too big - oh no!!!!!");
-			return false;
-		};
-		BufferedReader br = stringToBR(target.substring(start));
-		int c; 
-		int srcV = 0;
-		int destV;
-
-		while (((c = br.read()) != -1) && marker < target.length()) {
-			marker++;
-			char targetChar = (char)c;
-			System.out.println("marker: "+marker+", srcV: "+srcV+", char-c: "+targetChar);
-
-			Iterator<Edge> it = dfa.edgeIterator(srcV); // todo: reset iterator after successful find.
-
-			while(it.hasNext()){
-				Edge next = it.next();
-				System.out.println("hasnext: "+next);
-				if(next.contains(targetChar)){
-					System.out.println("woohoo! a match!: "+next);
-					srcV = next.getDest();
-					setStart(marker);
-					break;
-
-				}else{
-					System.out.println("oh, how sad: "+next+" marker: "+marker);
-					if(!eatString(dfa, target, marker)){
-						//reset(); // marker to 0
-						return false; // this means: it's too big - oh no!!!!!
-					} 
-				}
-			}
-
-			// if(!eatString(dfa, target, marker)){
-			// 	return false; // this means: it's too big - oh no!!!!!
-			// }
-		}
-		//reset(); // marker to 0
-		return true; // modify to true here, which means that target.length is still within marker
-	}
 	
-	private boolean findInEdge(){
-		return true;
-	}
-
 	// private helper methods:
 	private static BufferedReader stringToBR(String str){
 		InputStream is = new ByteArrayInputStream(str.getBytes());
 		BufferedReader br = new BufferedReader(new InputStreamReader(is));
 		return br;
 	}
-
-
-	private static String fileToString(String pathToFile) {
-        String result = null;
-        DataInputStream in = null;
-        try {
-            File f = new File(pathToFile);
-            byte[] buffer = new byte[(int) f.length()];
-            in = new DataInputStream(new FileInputStream(f));
-            in.readFully(buffer);
-            result = new String(buffer, "UTF-8");
-        } catch (IOException e) {
-            throw new RuntimeException("IO problem in fileToString", e);
-        } finally {
-            try {
-                in.close();
-            } catch (IOException e) { 
-                    e.printStackTrace();
-            }
-        }
-        return result;
-    }
     
 }
 
